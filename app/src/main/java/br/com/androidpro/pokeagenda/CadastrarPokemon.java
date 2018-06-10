2bbe8a76dc43eec59ade0d33ed319823eb1c978b
wesmaffazzolli <31376480+wesmaffazzolli@users.noreply.github.com>
2018-06-10 15:23:27 -0300

    Insert Pokemon and bug fixes

--- app/src/main/java/br/com/androidpro/pokeagenda/CadastrarPokemon.java
+++ app/src/main/java/br/com/androidpro/pokeagenda/CadastrarPokemon.java
@@ -1,18 +1,129 @@
 package br.com.androidpro.pokeagenda;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import static android.media.MediaRecorder.VideoSource.CAMERA;
 
 public class CadastrarPokemon extends AppCompatActivity {
+
+    private ImageView imagem;
+    EditText nome, especie, peso, altura;
+    private final int GALERIA_IMAGENS = 1;
+    private final int PERMISSAO_REQUEST = 2;
+    private final int TIRAR_FOTO = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_pokemon);
 
+        nome = (EditText) findViewById(R.id.nomeEditText);
+        especie = (EditText) findViewById(R.id.especieEditText);
+        peso = (EditText) findViewById(R.id.especieEditText);
+        altura = (EditText) findViewById(R.id.alturaEditText);
+        imagem = (ImageView) findViewById(R.id.imageViewCadastro);
+
+        //Código da flecha de botão voltar nativo do Android
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        //Método que faz a permissão para acessar a galeria de fotos
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        PERMISSAO_REQUEST);
+            }
+        }
+
+        //Código que realiza a chamada das funções relacionadas ao botão que seleciona imagens da galeria.
+        Button botaoSelecionar = (Button) findViewById(R.id.buttonSelecionarImagem);
+        botaoSelecionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, GALERIA_IMAGENS);
+            }
+        });
+
+        //Código que realiza a chamada das funções relacionadas ao botão que tira foto.
+        Button tirarFoto = (Button) findViewById(R.id.buttonTirarFoto);
+        tirarFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, TIRAR_FOTO);
+                }
+            }
+        });
+    }
+
+    protected void enviarFormularioCadastro(View view) {
+        if(!nome.getText().toString().isEmpty() && !especie.getText().toString().isEmpty() && !altura.getText().toString().isEmpty() && !peso.getText().toString().isEmpty()) {
+            if(imagem.getDrawable() != null) {
+
+            } else {
+                Toast.makeText(this, "Selecione ou tire uma foto!", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Toast.makeText(this, "Preencha todos os campos!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    //Código que recebe o retorno das funções de selecionar imagem da galeria ou tirar foto (RETORNO = OBJETO IMAGEM)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //Se o retorno vier da galeria de fotos, este é o código que trata o retorno.
+        if (resultCode == RESULT_OK && requestCode == GALERIA_IMAGENS) {
+            Uri selectedImage = data.getData();
+            String[] filePath = { MediaStore.Images.Media.DATA };
+            Cursor c = getContentResolver().query(selectedImage,filePath, null, null, null);
+            c.moveToFirst();
+            int columnIndex = c.getColumnIndex(filePath[0]);
+            String picturePath = c.getString(columnIndex);
+            c.close();
+            Bitmap imagemGaleria = (BitmapFactory.decodeFile(picturePath));
+
+            //Adaptar/redimensionar o tamanho da imagem para o tamanho da tela do dispositivo.
+            //Resolve o problema do erro: OpenGLRenderer: Bitmap too large to be uploaded into a texture (2432x4320, max=4096x4096)
+            int scale = (int)(imagemGaleria.getHeight() * (512.0 / imagemGaleria.getWidth()));
+            Bitmap scaled = Bitmap.createScaledBitmap(imagemGaleria, 512, scale, true);
+
+            //imagem.setImageBitmap(imagemGaleria);
+            imagem.setImageBitmap(scaled);
+        }
+
+        //Se o retorno vier da câmera, este é o código que trata o retorno.
+        if (requestCode == TIRAR_FOTO && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imagem.setImageBitmap(imageBitmap);
+        }
     }
 
     @Override
@@ -24,5 +135,19 @@ public class CadastrarPokemon extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        if (requestCode == PERMISSAO_REQUEST) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // A permissão foi concedida. Pode continuar
+            } else {
+                // A permissão foi negada. Precisa ver o que deve ser desabilitado
+            }
+            return;
+        }
     }
 }
